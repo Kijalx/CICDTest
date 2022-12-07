9e1ad116275ab0cf69ad95f1cf87b97f6b275994
Aleksander Kijewski <G00396018@atu.ie>
2022-12-07 11:16:37 +0000

    Created Variables

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,8 @@
 public class Employee {
-    
+    private String Name;
+    private String PPSID;
+    private int PhoneNumber;
+    private String Gender;
+    private String EmploymentType;
+    private int Age;
 }
